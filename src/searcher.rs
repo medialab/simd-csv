35b7b4c3305a7ecd732279d19c8a942658c1d2fa
Yomguithereal <guillaumeplique@gmail.com>
2025-09-23 17:53:37 +0200

    aarch64 draft implementation

--- src/searcher.rs
+++ src/searcher.rs
@@ -15,28 +15,6 @@ impl<T> Pointer for *const T {
     }
 }
 
-#[inline(always)]
-fn get_for_offset(mask: u32) -> u32 {
-    #[cfg(target_endian = "big")]
-    {
-        mask.swap_bytes()
-    }
-    #[cfg(target_endian = "little")]
-    {
-        mask
-    }
-}
-
-#[inline(always)]
-fn first_offset(mask: u32) -> usize {
-    get_for_offset(mask).trailing_zeros() as usize
-}
-
-#[inline(always)]
-fn clear_least_significant_bit(mask: u32) -> u32 {
-    mask & (mask - 1)
-}
-
 #[cfg(target_arch = "x86_64")]
 mod x86_64 {
     use core::arch::x86_64::{
@@ -44,7 +22,29 @@ mod x86_64 {
     };
     use std::marker::PhantomData;
 
-    use super::{clear_least_significant_bit, first_offset, Pointer};
+    use super::Pointer;
+
+    #[inline(always)]
+    fn get_for_offset(mask: u32) -> u32 {
+        #[cfg(target_endian = "big")]
+        {
+            mask.swap_bytes()
+        }
+        #[cfg(target_endian = "little")]
+        {
+            mask
+        }
+    }
+
+    #[inline(always)]
+    fn first_offset(mask: u32) -> usize {
+        get_for_offset(mask).trailing_zeros() as usize
+    }
+
+    #[inline(always)]
+    fn clear_least_significant_bit(mask: u32) -> u32 {
+        mask & (mask - 1)
+    }
 
     #[derive(Debug)]
     pub struct SSE2Searcher {
@@ -164,18 +164,174 @@ mod x86_64 {
     }
 }
 
-// #[cfg(target_arch = "aarch64")]
-// mod aarch64 {
-//     pub struct NeonSearcher {}
-// }
+#[cfg(target_arch = "aarch64")]
+mod aarch64 {
+    use core::arch::aarch64::{
+        uint8x16_t, vceqq_u8, vdupq_n_u8, vget_lane_u64, vld1q_u8, vorrq_u8, vreinterpret_u64_u8,
+        vreinterpretq_u16_u8, vshrn_n_u16,
+    };
+    use std::marker::PhantomData;
+
+    use super::Pointer;
+
+    #[inline(always)]
+    fn neon_movemask(v: uint8x16_t) -> u64 {
+        let asu16s = vreinterpretq_u16_u8(v);
+        let mask = vshrn_n_u16(asu16s, 4);
+        let asu64 = vreinterpret_u64_u8(mask);
+        let scalar64 = vget_lane_u64(asu64, 0);
+
+        scalar64 & 0x8888888888888888
+    }
+
+    #[inline(always)]
+    fn get_for_offset(mask: u64) -> u64 {
+        #[cfg(target_endian = "big")]
+        {
+            mask.swap_bytes()
+        }
+        #[cfg(target_endian = "little")]
+        {
+            mask
+        }
+    }
+
+    #[inline(always)]
+    fn first_offset(mask: u64) -> usize {
+        (get_for_offset(mask).trailing_zeros() >> 2) as usize
+    }
+
+    #[inline(always)]
+    fn clear_least_significant_bit(mask: u64) -> u64 {
+        mask & (mask - 1)
+    }
+
+    #[derive(Debug)]
+    pub struct NeonSearcher {
+        n1: u8,
+        n2: u8,
+        n3: u8,
+        v1: uint8x16_t,
+        v2: uint8x16_t,
+        v3: uint8x16_t,
+    }
+
+    impl NeonSearcher {
+        #[inline]
+        pub unsafe fn new(n1: u8, n2: u8, n3: u8) -> Self {
+            Self {
+                n1,
+                n2,
+                n3,
+                v1: vdupq_n_u8(n1 as i8),
+                v2: vdupq_n_u8(n2 as i8),
+                v3: vdupq_n_u8(n3 as i8),
+            }
+        }
+
+        #[inline(always)]
+        pub fn iter<'s, 'h>(&'s self, haystack: &'h [u8]) -> NeonIndices<'s, 'h> {
+            NeonIndices::new(self, haystack)
+        }
+    }
+
+    #[derive(Debug)]
+    pub struct NeonIndices<'s, 'h> {
+        searcher: &'s NeonSearcher,
+        haystack: PhantomData<&'h [u8]>,
+        start: *const u8,
+        end: *const u8,
+        current: *const u8,
+        mask: u64,
+    }
+
+    impl<'s, 'h> NeonIndices<'s, 'h> {
+        #[inline]
+        fn new(searcher: &'s NeonSearcher, haystack: &'h [u8]) -> Self {
+            let ptr = haystack.as_ptr();
+
+            Self {
+                searcher,
+                haystack: PhantomData,
+                start: ptr,
+                end: ptr.wrapping_add(haystack.len()),
+                current: ptr,
+                mask: 0,
+            }
+        }
+    }
+
+    const SSE2_STEP: usize = 16;
+
+    impl<'s, 'h> NeonIndices<'s, 'h> {
+        pub unsafe fn next(&mut self) -> Option<usize> {
+            if self.start >= self.end {
+                return None;
+            }
+
+            let mut mask = self.mask;
+            let vectorized_end = self.end.sub(SSE2_STEP);
+            let mut current = self.current;
+            let start = self.start;
+            let v1 = self.searcher.v1;
+            let v2 = self.searcher.v2;
+            let v3 = self.searcher.v3;
+
+            'main: loop {
+                // Processing current move mask
+                if mask != 0 {
+                    let offset = current.sub(SSE2_STEP).add(first_offset(mask));
+                    self.mask = clear_least_significant_bit(mask);
+                    self.current = current;
+
+                    return Some(offset.distance(start));
+                }
+
+                // Main loop of unaligned loads
+                while current <= vectorized_end {
+                    let chunk = vld1q_u8(current);
+                    let cmp1 = vceqq_u8(chunk, v1);
+                    let cmp2 = vceqq_u8(chunk, v2);
+                    let cmp3 = vceqq_u8(chunk, v3);
+                    let cmp = vorrq_u8(cmp1, cmp2);
+                    let cmp = vorrq_u8(cmp, cmp3);
+
+                    mask = neon_movemask(cmp);
+
+                    current = current.add(SSE2_STEP);
+
+                    if mask != 0 {
+                        continue 'main;
+                    }
+                }
+
+                // Processing remaining bytes linearly
+                while current < self.end {
+                    if *current == self.searcher.n1
+                        || *current == self.searcher.n2
+                        || *current == self.searcher.n3
+                    {
+                        let offset = current.distance(start);
+                        self.current = current.add(1);
+                        return Some(offset);
+                    }
+                    current = current.add(1);
+                }
+
+                return None;
+            }
+        }
+    }
+}
 
 #[derive(Debug)]
 pub struct Searcher {
     #[cfg(target_arch = "x86_64")]
     inner: x86_64::SSE2Searcher,
 
-    // #[cfg(target_arch = "aarch64")]
-    // inner: aarch64::NeonSearcher,
+    #[cfg(target_arch = "aarch64")]
+    inner: aarch64::NeonSearcher,
+
     #[cfg(not(any(target_arch = "x86_64", target_arch = "aarch64")))]
     inner: memchr::arch::all::memchr::Three,
 }
@@ -187,6 +343,16 @@ impl Searcher {
         unsafe {
             Self {
                 inner: x86_64::SSE2Searcher::new(n1, n2, n3),
+            }
+        }
+    }
+
+    #[inline(always)]
+    #[cfg(target_arch = "aarch64")]
+    pub fn new(n1: u8, n2: u8, n3: u8) -> Self {
+        unsafe {
+            Self {
+                inner: aarch64::NeonSearcher::new(n1, n2, n3),
             }
         }
     }
@@ -208,6 +374,14 @@ impl Searcher {
     }
 
     #[inline(always)]
+    #[cfg(target_arch = "aarch64")]
+    pub fn search<'s, 'h>(&'s self, haystack: &'h [u8]) -> Indices<'s, 'h> {
+        Indices {
+            inner: self.inner.iter(haystack),
+        }
+    }
+
+    #[inline(always)]
     #[cfg(not(any(target_arch = "x86_64", target_arch = "aarch64")))]
     pub fn search<'s, 'h>(&'s self, haystack: &'h [u8]) -> Indices<'s, 'h> {
         Indices {
@@ -221,6 +395,9 @@ pub struct Indices<'s, 'h> {
     #[cfg(target_arch = "x86_64")]
     inner: x86_64::SSE2Indices<'s, 'h>,
 
+    #[cfg(target_arch = "aarch64")]
+    inner: aarch64::SSE2Indices<'s, 'h>,
+
     #[cfg(not(any(target_arch = "x86_64", target_arch = "aarch64")))]
     inner: memchr::arch::all::memchr::ThreeIter<'s, 'h>,
 }
@@ -230,6 +407,12 @@ impl<'s, 'h> Iterator for Indices<'s, 'h> {
 
     #[inline(always)]
     #[cfg(target_arch = "x86_64")]
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe { self.inner.next() }
+    }
+
+    #[inline(always)]
+    #[cfg(target_arch = "aarch64")]
     fn next(&mut self) -> Option<Self::Item> {
         unsafe { self.inner.next() }
     }
