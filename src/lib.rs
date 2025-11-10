89e72cd9ceecbea79066269595cfdc646ef28830
Yomguithereal <guillaumeplique@gmail.com>
2025-11-10 14:53:57 +0100

    Design notes

--- src/lib.rs
+++ src/lib.rs
@@ -102,6 +102,11 @@ You can also find more exotic readers like:
 - Everywhere else, the library will fallback to [SWAR](https://en.wikipedia.org/wiki/SWAR)
   techniques or scalar implementations.
 
+Using `RUSTFLAGS='-C target-cpu=native'` should not be required when compiling
+this crate because it either uses SIMD instructions tied to your `target_arch`
+already and because it will rely on runtime detection to find better SIMD
+instructions (typically `avx2`).
+
 # Design notes
 
 ## Regarding performance
@@ -109,19 +114,21 @@ You can also find more exotic readers like:
 This crate's CSV parser has been cautiously designed to offer "reasonable" performance
 by combinining traditional state machine logic with SIMD-accelerated string searching.
 
-I say "reasonable" because you cannot expect to go 16/32 times faster than a state-of-the-art
+I say "reasonable" because you cannot expect to parse 16/32 times faster than a state-of-the-art
 scalar implementation like the [`csv`](https://docs.rs/csv/) crate. What's more, the
 throughput of the SIMD-accelerated parser remains very data-dependent. Sometimes
 you will go up to ~8 times faster, sometimes you will only go as fast as scalar code.
+(Remember also that CSV parsing is often an IO-bound task, even more so than with other
+data formats expected to fit into memory like JSON etc.)
 
 As a rule of thumb, the larger your records and cells, the greater the
-performance boost vs. a scalar byte-by-byte implementation. This also means that
-for worst cases, this crate's parser will just be on par with scalar code, but I
+performance boost vs. a scalar byte-by-byte implementation will be. This also means that
+for worst cases, this crate's parser will just be on par with scalar code. I
 have made everything in my power to ensure this SIMD parser is never slower (I think
 one of the reasons why SIMD CSV parsers are not yet very prevalent is that they
 tend to suffer real-life cases where scalar code outperform them).
 
-Also, note that this crate is geared towards parsing streams of CSV data that is
+Also, note that this crate is geared towards parsing **streams** of CSV data
 only quoted when needed (e.g. not written with a `QUOTE_ALWAYS` policy).
 
 ## Regarding simdjson techniques
@@ -130,13 +137,14 @@ I have tried very hard to apply [simdjson](https://arxiv.org/abs/1902.08318) tri
 to make this crate's parser as branchless as possible but I couldn't make it as
 fast as the state-machine/SIMD string searching hybrid.
 
-`PCLMULQDQ` tricks in this context only adds more complexity and overhead to the
-SIMD sections of the code, all while making it less "democratic" since you need
-specific SIMD instructions that are not available everywhere.
+`PCLMULQDQ` & shuffling tricks in this context only add more complexity and overhead
+to the SIMD sections of the code, all while making it less "democratic" since you need
+specific SIMD instructions that are not available everywhere, if you don't want to
+fallback to scalar instructions.
 
 Said differently, those techniques seem overkill in practice for CSV parsing.
 But it is also possible I am not competent enough to make them work properly and
-I won't hesitate to move towards them if I am proven wrong.
+I won't hesitate to move towards them if proven wrong.
 
 ## Hybrid design
 
@@ -152,12 +160,47 @@ was found using typical bit-twiddling.
 
 This ultimately means that branching happens on each structural characters rather
 than on each byte, which is very good. But this is also the reason why CSV data
-with a very high density of structural characters will not get parsed that much
-faster than using scalar code only.
+with a very high density of structural characters will not get parsed much faster
+than with the equivalent scalar code.
 
 ## Two-speed SIMD branches
 
+This crate's CSV parser actually uses two different modes of SIMD string searching:
+
+1. when reading unquoted CSV data, the parser uses an amortized variant of the
+[`memchr`](https://docs.rs/memchr/latest/memchr/) routines where move masks
+containing more than a single match are kept and consumed progressively on subsequent calls,
+instead of restarting a search from the character just next to an earlier match, as the
+`memchr_iter` routine does.
+2. when reading quoted CSV data, the parser uses the optmized & unrolled functions
+of the [`memchr`](https://docs.rs/memchr/latest/memchr/) crate directly to find the next
+quote as fast as possible.
+
+This might seem weird but this seems to be the best tradeoff for performance. Counter-intuitively,
+using larger SIMD registers like `avx2` for 1. actually hurts the overall performance.
+Similarly, using the amortized routine to scan quoted data is actually slower than
+using the unrolled functions of [`memchr`](https://docs.rs/memchr/latest/memchr/).
+
+This actually makes sense if you consider that the longer a field is, the more
+probable it is to contain a character requiring the field to be quoted. What's more
+the density of quotes to be found in a quoted field is usually lower that structural
+characters in an unquoted CSV stream. So if you use larger SIMD registers in the
+unquoted stream you will end up 1. throttling the SIMD part of the code too much
+because of the inner branching (when hitting a delimiter or a newline) and 2. you
+will often discard too much work when hitting a record end or a quoted field.
+
 ## Copy amortization
+
+Copying tiny amounts of data often is quite detrimental to the overall performance.
+As such, and to make sure the copying [`Reader`] remains as fast as possible,
+I decided to change the design of the [`ByteRecord`] to save fields as fully-fledged
+ranges over the underlying byte slice instead of only delimiting them implicitly
+by the offsets separating them as it is done in the [`csv`](https://docs.rs/csv/) crate.
+
+This means I am able to copy large swathes of unquoted data at once instead of
+copying fields one by one. This also means I keep delimiter characters and sometimes
+inconsequential double quotes in the underlying byte slice (but don't worry, the
+user will never actually see them), so that copies remain as vectorized as possible.
 
 # Caveats
 
